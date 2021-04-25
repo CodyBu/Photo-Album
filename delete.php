068c4b0a2f3966e59f1f917fb934f3a7d12f82e8
duongle1 <80299392+duongle1@users.noreply.github.com>
2021-04-25 18:31:09 -0400

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,23 @@
+<html>
+
+    <body>
+        <?php
+            $id = $_GET['id'];
+            include('connection.php');
+
+            $sql = "delete from users where user_Id = '$id'";
+
+            if($conn->query($sql) == TRUE){
+                echo "Record deleted";
+
+            }
+
+            else{
+                echo "Error: ".$sql."<br>".$conn->error;
+            }
+
+            header("location:update-data.php");
+        ?>
+
+    </body>
+</html>
---
+++ update.php
@@ -0,0 +1,74 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+        <?php
+            $id = $_GET['id'];
+            /*include('connection.php');
+
+            $sql = "select * from users where user_Id = '$id'";
+            $result = mysqli_query($sql);
+            $row = mysqli_fetch_array($result);*/
+            
+        ?>
+        <form name = "form1" method = "post" action = "exec_update.php" enctype = "multipart/form-data">
+
+            <input type = "hidden" name = "id" value ="<?php echo $id;?>">
+
+            <table with = "356" border = "1" class = "table">
+                <tr>
+                    <td colspan = "2"><div align = "center">User Registration form</div></td>
+                </tr>
+
+                <tr>
+                    <td colspan = "2">
+                        <?php
+                            error_reporting(0);
+                            echo $_GET['msg'];
+                        ?>
+                    
+                    </td>
+
+                </tr>
+
+                <tr>
+                    <td width = "178">Name</td>
+                    <td><input type = "text" name = "name"></td>
+
+                </tr>
+
+                <tr>
+                    <td width = "178">Email</td>
+                    <td><input type = "text" name = "email"></td>
+
+                </tr>
+
+                <tr>
+                    <td width = "178">Password</td>
+                    <td><input type = "text" name = "password"></td>
+
+                </tr>
+
+                <tr>
+                    <td width = "178">Your picture</td>
+                    <td><input type = "file" name = "pic"></td>
+
+                </tr>
+
+                <tr>
+                    <td colspan = "2">
+                        <div>
+                            <input type = "submit" name = "submit" value =  "Register">
+                        </div>
+                    </td>
+
+                </tr>
+
+
+            </table>
+        </form>
+    </body>
+</html>
