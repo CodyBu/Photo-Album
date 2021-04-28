5f076293aac6d9b2cbba8e0a9818aaf48d666ffe
duongle1 <80299392+duongle1@users.noreply.github.com>
2021-04-28 08:47:54 -0400

    login_action.php is done

---
+++ login_action.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+    $email = $_GET['email'];
+    $password = $_GET['password'];
+        
+
+    include("connection.php");
+
+    $sql = "SELECT * FROM users where User_Email = '$email' and User_Password = '$password'";
+
+    $results = mysqli_query($conn, $sql);
+    $users = mysqli_num_rows($results);
+    $row = mysqli_fetch_array($results);
+
+
+    $_SESSION['name'] = $row['User_Name'];
+    $_SESSION['email'] = $row['User_Email'];
+    $_SESSION['dob'] = $row['User_Dob'];
+    $_SESSION['dom'] = $row['User_Dom'];
+    $_SESSION['pic'] = $row['User_Picture'];
+
+    if($users >0){
+        include("nav.php");
+        include("login_function.php");
+    }
+    
+    else{
+        header("location:index.php?msg=User not found");
+    }
+?>
