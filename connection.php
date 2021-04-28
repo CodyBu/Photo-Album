461da0670f6b403eb36bacc21770e68e7796efc7
duongle1 <80299392+duongle1@users.noreply.github.com>
2021-04-28 08:35:47 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -17,28 +17,7 @@
             //connect to database:
             mysqli_select_db($conn, "workdatabase1") or die ("Could not connect to the database"); //workdatabase1: name of database
             echo "Connect to the dadabase successfully";
-/*<body>
-            
-            <?php
-                    $conn = mysqli_connect("localhost", "root", "root");
 
-                    if(!$conn) {
-
-                        die("Connection failed: " . mysqli_connect_error());
-                    }
-                    else {
-
-                        echo "Connected successfully";
-                    }
-
-                    mysqli_select_db($conn, "ourWorkDB1") or die("Could not connect to the database");
-                    echo "Connected to the database successfully";
-
-            ?>
-
-</body>
-
-</html>*/
         ?>
 
         
