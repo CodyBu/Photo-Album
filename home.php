a8bf91bdf0636f7bdbf4925a1b69767a63bb2ac6
EduardAnt <80110441+EduardAnt@users.noreply.github.com>
2021-04-29 15:14:39 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+   </head>
+ <body style="background-color:#99CCFF;"> 
+        <?php
+                include ("nav.php");
+        ?>
+        
+    <div class = "container" style = "padding-top:200px" background="blue">
+        <div class = "jumbotron" >
+       <center>
+           
+              <h1 style="color:blue">Welcome to Photo Album website</h1>
+              <h5 style = "color:#000099">You can create your photo album</h5>
+              <br>
+              <p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;by Cody, Le, Andrew and Eduard</P
+                     
+       </center>          
+        </div>'
+    </div>
+</body> 
