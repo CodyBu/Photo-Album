7623d527c6e964da05eec8ed4b04ef3da0d22378
3dsurfer <57121418+3dsurfer@users.noreply.github.com>
2021-04-29 23:48:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
     <body>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#"><b>Album App</b></a>
+            <a class="navbar-brand" href="index.php"><b>Album App</b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
