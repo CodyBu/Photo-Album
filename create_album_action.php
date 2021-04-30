8abf2a2984070c7179487c962487713a9752905b
3dsurfer <57121418+3dsurfer@users.noreply.github.com>
2021-04-29 21:14:47 -0400

    Update create_album_action.php

--- create_album_action.php
+++ create_album_action.php
@@ -1,24 +1,23 @@
 <?php
     ob_start();
     session_start();
-
     $title = $_POST['title'];
     $description = $_POST['description'];
-    $album = $_SESSION['album_id'];
+    $user = $_SESSION['user_id'];
 
-    $filename = $_FILES['pic']['name'];
+    $filename = $_FILES['pic']['name'];           
     $tmpname = $_FILES['pic']['tmp_name'];
 
-    $destination = "uploads/pictures/".rand().$filename;
+    $destination = "uploads/cover-photos/".rand().$filename;        //generate random number & concatinate with filename & store in folder "uploads"
     move_uploaded_file($tmpname,$destination);
 
     include("connection.php");
 
-    $sql = "INSERT INTO PICTURES (Album_Id, Picture_Image, Picture_Title, Picture_Note) VALUES ($album, '$destination', '$title', '$description');";
+    $sql = "INSERT INTO ALBUM (User_Id, Album_Cover, Album_Title, Album_Notes) VALUES ($user, '$destination', '$title', '$description');";
 
     if($conn->query ($sql) == TRUE){
 
-        header("location:create_picture.php?msg=Picture Uploaded!");
+        header("location:create_album.php?msg=Album Created!");   
 
     }
 
