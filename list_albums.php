b8448edb94fca56480ba692207441d7710a4beb8
EduardAnt <80110441+EduardAnt@users.noreply.github.com>
2021-05-01 01:27:19 -0400

    Add files via upload

--- list_albums.php
+++ list_albums.php
@@ -24,30 +24,32 @@
 
                     <div class = "form-group">
                         <tr>
-                            <td colspan = "4" align = "center"><h2>Albums</h2></td>
+                            <td colspan = "5" align = "center"><h2>Albums</h2></td>
                         </tr>
                     </div>
 
                     <div class = "form-group">
-                        <tr>
-                            <th>Album</th>
-                            <th>Description</th>
-                            <th>View</th>
+                        <tr align="center">
+                            <th style="width:20%">Title</th>
+                            <th style="width:20%">Cover Picture</th>
+                            <th style="width:20%">Description</th>
+                            <th style="width:20%">View</th>
                             <th colspan = '2'>Action</td>
                         </tr>
                         <form id = "albumForm" action = "view_album.php" method = "post">
                             <?php
                                 include("connection.php");
 
-                                $sql = "SELECT * FROM ALBUM WHERE User_Id = " . $_SESSION['user_id'];
+                                $sql = "SELECT * FROM ALBUM WHERE User_id = " . $_SESSION['user_id'];
                             
                                 $results = mysqli_query($conn, $sql);
 
                                 while($row = mysqli_fetch_array($results)){
-                                    echo "<tr><td>"."<br><img style='height: 100px; width: 100px;' src='" . $row['Album_Cover'] . "'></td>";
-                                    echo "<td>" . $row['Album_Notes'] . "</td>";
-                                    echo "<td><input type='radio' class='btn-check' name='selected' value='" . $row['Album_Id'] . "' id='" . $row['Album_Id'] . "' onclick='submit()'><label class='btn btn-primary' for='" . $row['Album_Id'] . "'>View</label></td>";
-                                    echo "<td colspan = '2'><a href = delete-album.php?id=";echo $row[0];echo">Delete | </a>";
+                                    echo "<tr><td align = 'center'>".$row[3]."</td>";
+                                    echo "<td align = 'center'>"."<img style='height: 100px; width: 100px;' src='" . $row['Album_Cover'] . "'></td>";
+                                    echo "<td align = 'center'>" . $row['Album_Notes'] . "</td>";
+                                    echo "<td align = 'center'><input type='radio' class='btn-check' name='selected' value='" . $row['Album_Id'] . "' id='" . $row['Album_Id'] . "' onclick='submit()'><label class='btn btn-primary' for='" . $row['Album_Id'] . "'>View</label></td>";
+                                    echo "<td colspan = '2' align = 'center'><a href = delete-album.php?id=";echo $row[0];echo">Delete | </a>";
                                     echo "<a href = update-album.php?id=";echo $row[0];echo">Update</a></td></tr>";    
 
                                 };
@@ -56,9 +58,7 @@
                         </form>
                     </div>
                 </table>
-                <div>
-                        <h6 align ="center">Upload pictures to the album press: <a href = "create_picture.php">here</a></h6>
-                </div>
+
         </div>
     </body>
 </html>
